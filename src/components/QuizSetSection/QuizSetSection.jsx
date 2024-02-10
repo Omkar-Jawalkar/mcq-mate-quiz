ad62d2ee2eb5c0c12ab124f0461fdd69866d1dd0
prajwal89 <prajwalhallale89@gmail.com>
2024-02-10 13:12:51 +0530

    corrected var name

--- src/components/QuizSetSection/QuizSetSection.jsx
+++ src/components/QuizSetSection/QuizSetSection.jsx
@@ -4,7 +4,7 @@ import RandomSeries from "./RandomSeries";
 import { useState } from "react";
 
 const QuizSetSection = ({ sets, attempts }) => {
-    const [tabOpened, setOpenedTab] = useState('test-series');
+    const [openedTab, setOpenedTab] = useState('test-series');
 
     return (
         <div className="w-full p-2 border-b justify-evenly">
@@ -12,13 +12,13 @@ const QuizSetSection = ({ sets, attempts }) => {
             {/* todo disable this navigation once quiz is started*/}
             <div className="space-x-2">
                 <button
-                    className={`py-2 text-xl ${tabOpened === 'test-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
+                    className={`py-2 text-xl ${openedTab === 'test-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
                     onClick={() => setOpenedTab('test-series')}
                 >
                     Test Series
                 </button>
                 <button
-                    className={`py-2 text-xl ${tabOpened === 'random-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
+                    className={`py-2 text-xl ${openedTab === 'random-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
                     onClick={() => setOpenedTab('random-series')}
                 >
                     Random Mcqs
@@ -26,8 +26,8 @@ const QuizSetSection = ({ sets, attempts }) => {
             </div>
 
             <div className="px-2 py-4 border rounded-b-md border-primary-600/40">
-                {tabOpened === 'test-series' && <TestSeries sets={sets} attempts={attempts} />}
-                {tabOpened === 'random-series' && <RandomSeries />}
+                {openedTab === 'test-series' && <TestSeries sets={sets} attempts={attempts} />}
+                {openedTab === 'random-series' && <RandomSeries />}
             </div>
         </div>
     );
