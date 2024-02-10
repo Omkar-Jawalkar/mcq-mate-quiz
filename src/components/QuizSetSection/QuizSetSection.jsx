6d2bf4d6ff70e093bc1e8db7122dacde9d273fe3
prajwal89 <prajwalhallale89@gmail.com>
2024-02-10 11:49:42 +0530

    Tab navigation

--- src/components/QuizSetSection/QuizSetSection.jsx
+++ src/components/QuizSetSection/QuizSetSection.jsx
@@ -1,12 +1,31 @@
 import PropTypes from "prop-types";
 import TestSeries from "./TestSeries";
 import RandomSeries from "./RandomSeries";
+import { useState } from "react";
 
 const QuizSetSection = ({ sets, attempts }) => {
+    const [tabOpened, setOpenedTab] = useState('test-series');
+
     return (
-        <div className="flex w-full py-6 justify-evenly border-b border-black/25 ">
-            <TestSeries sets={sets} attempts={attempts} />
-            <RandomSeries />
+        <div className="w-full p-2 border-b justify-evenly">
+            <div className="mb-4 space-x-2 ">
+                <button
+                    className={`py-2 text-xl ${tabOpened === 'test-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
+                    onClick={() => setOpenedTab('test-series')}
+                >
+                    Test Series
+                </button>
+                <button
+                    className={`py-2 text-xl ${tabOpened === 'random-series' ? 'border-b-2 border-primary-600' : 'border-none'} `}
+                    onClick={() => setOpenedTab('random-series')}
+                >
+                    Random Mcqs
+                </button>
+            </div>
+            <div>
+                {tabOpened === 'test-series' && <TestSeries sets={sets} attempts={attempts} />}
+                {tabOpened === 'random-series' && <RandomSeries />}
+            </div>
         </div>
     );
 };
